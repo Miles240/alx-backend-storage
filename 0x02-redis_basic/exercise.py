eaa138b09f78d47523874d6bd3185d7f46d3711a
bg-miles <piuschukwuebuka40@gmail>
2024-08-14 22:45:08 +0100

    callable methods

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -25,3 +25,28 @@ class Cache:
         key = str(uuid.uuid1())
         self._redis.set(key, data)
         return key
+
+    def get(
+        self, key: str, fn: Optional[Callable] = None
+    ) -> Union[str, int, bytes, float]:
+        """
+        Retrieves data from Redis and applies an optional function to transform the data.
+        Args:
+            key(str): The key to retrieve data from Redis.
+            fn(Optional[Callable]): A function to apply to the retrieved data.
+        Returns:
+            Transformed data or the raw data if no function is provided.
+        """
+
+        data = self._redis.get(key)
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        """Retrives a string format"""
+        return self.get(key, str)
+
+    def get_int(self, key: str) -> int:
+        """Retrives an int format"""
+        return self.get(key, int)
